d10ae299a332cba18136907f9263474702e632cf
KhoyrurRoykhan <khoirurroykan007@gmail.com>
2025-05-14 15:59:16 +0800

    update verify

--- middleware/VerifyToken.js
+++ middleware/VerifyToken.js
@@ -6,6 +6,7 @@ export const verifyToken = (req, res, next) => {
     if(token == null) return res.sendStatus(401);
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if(err) return res.sendStatus(403);
+        req.userId = decoded.userId; // tambahkan ini
         req.email = decoded.email;
         req.token_kelas = decoded.token_kelas; // simpan token_kelas
         next();
